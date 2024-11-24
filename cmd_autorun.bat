0a8dede85c40267631fea886a610989e4768ecfd
huuthienp <htp898@uowmail.edu.au>
2024-11-24 22:16:01 +1100

    feat: Add cmd aliases

--- cmd_autorun.bat
+++ cmd_autorun.bat
@@ -4,11 +4,3 @@ set EDITOR=%USERPROFILE%\scoop\shims\lvim.cmd
 
 call %USERPROFILE%\etc\cmd_prompt.bat
 call %USERPROFILE%\etc\cmd_aliases.bat
-
-doskey cd=cd $* $T call %USERPROFILE%\etc\prompt_dir.cmd
-doskey pushd=pushd $* $T call %USERPROFILE%\etc\prompt_dir.cmd
-doskey popd=popd $* $T call %USERPROFILE%\etc\prompt_dir.cmd
-
-doskey ll=dir $*
-doskey ls=dir /d $*
-doskey less=sh -c 'less $*'
