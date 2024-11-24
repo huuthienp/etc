c2500d7221e176bf06156560d91835f6335a8bb1
huuthienp <htp898@uowmail.edu.au>
2024-11-24 22:05:09 +1100

    feat: Make cmd prompt git-branch-aware

--- cmd_prompt.bat
+++ cmd_prompt.bat
@@ -1,17 +1,26 @@
 @echo off
 
+set _temp=temp.txt
+
 for %%i in ("%cd%") do set "cwd=%%~nxi"
 
 if defined SSH_CLIENT (
-  set "_uh=%USERNAME%@%COMPUTERNAME%"
+  set "_uh=%USERNAME%@%COMPUTERNAME% "
 ) else (
   if defined SSH_TTY (
-    set "_uh=%USERNAME%@%COMPUTERNAME%"
+    set "_uh=%USERNAME%@%COMPUTERNAME% "
   ) else (
     set "_uh="
   )
 )
-rem echo %_uh%
 
-set "ps1=$m$_%_uh%($n:\) $e[1;34m%cwd% $e[00m$+$$$s"
+git branch 2>null | findstr /b "* " > %_temp% || set __git_branch= && goto :setprompt
+set /p __git_branch=<%_temp%
+set "__git_branch=%__git_branch: =%"
+set "__git_branch=%__git_branch:**=%"
+set "__git_branch=(%__git_branch%)"
+
+:setprompt
+del %_temp%
+set "ps1=$m$_%_uh%$n:\ $e[1;34m%cwd% $+$e[00m%__git_branch% $$$s"
 prompt %ps1%
