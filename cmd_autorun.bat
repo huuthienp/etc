80e048a67baef7e8237871ab17aee950d898da50
huuthienp <htp898@uowmail.edu.au>
2024-11-24 22:14:53 +1100

    rename batchfiles

--- cmd_autorun.bat
+++ cmd_autorun.bat
@@ -1,6 +1,9 @@
 @echo off
 
-call %USERPROFILE%\etc\prompt_dir.cmd
+set EDITOR=%USERPROFILE%\scoop\shims\lvim.cmd
+
+call %USERPROFILE%\etc\cmd_prompt.bat
+call %USERPROFILE%\etc\cmd_aliases.bat
 
 doskey cd=cd $* $T call %USERPROFILE%\etc\prompt_dir.cmd
 doskey pushd=pushd $* $T call %USERPROFILE%\etc\prompt_dir.cmd
@@ -9,5 +12,3 @@ doskey popd=popd $* $T call %USERPROFILE%\etc\prompt_dir.cmd
 doskey ll=dir $*
 doskey ls=dir /d $*
 doskey less=sh -c 'less $*'
-
-set EDITOR=%USERPROFILE%\scoop\shims\lvim.cmd
