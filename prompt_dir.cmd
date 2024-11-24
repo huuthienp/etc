dabfc3d296eab4aab2817c65f311617af275eb21
huuthienp <htp898@uowmail.edu.au>
2024-11-24 19:58:45 +1100

    feat: Make CMD prompt ssh-aware

--- prompt_dir.cmd
+++ prompt_dir.cmd
@@ -1,3 +1,17 @@
 @echo off
+
 for %%i in ("%cd%") do set "cwd=%%~nxi"
-prompt %cwd%$G 
+
+if defined SSH_CLIENT (
+  set "_uh=%USERNAME%@%COMPUTERNAME%"
+) else (
+  if defined SSH_TTY (
+    set "_uh=%USERNAME%@%COMPUTERNAME%"
+  ) else (
+    set "_uh="
+  )
+)
+rem echo %_uh%
+
+set "ps1=$m$_%_uh%($n:\) $e[1;34m%cwd% $e[00m$+$$$s"
+prompt %ps1%
