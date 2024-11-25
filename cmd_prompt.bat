34967122b6e83a32520c1b8377ff6a4da9378e6e
huuthienp <htp898@uowmail.edu.au>
2024-11-25 14:59:11 +1100

    chore: make error handling easier to maintain

--- cmd_prompt.bat
+++ cmd_prompt.bat
@@ -14,7 +14,12 @@ if defined SSH_CLIENT (
   )
 )
 
-git branch 2>NUL | call findstr /b "* " > %_temp% || set "__git_branch=" && goto :setprompt
+git branch 2>NUL | call findstr /b "* " > %_temp%
+
+rem echo errorlevel is %errorlevel%
+if errorlevel 1 (set "__git_branch=")
+if errorlevel 1 goto :setprompt
+
 set /p __git_branch=<%_temp%
 set "__git_branch=%__git_branch: =%"
 set "__git_branch=%__git_branch:**=%"
