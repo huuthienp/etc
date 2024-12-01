4fde17c1f54199525ea2fa882cb929833fcf804a
huuthienp <htp898@uowmail.edu.au>
2024-12-02 01:26:29 +1100

    chore: Make branch-finding less visible

--- cmd_prompt.bat
+++ cmd_prompt.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set _temp=temp.txt
+set __git_branch_temp=".temp_branch_%RANDOM%"
 
 for %%G in ("%CD%") do set "cwd=%%~nxG"
 
@@ -14,18 +14,21 @@ if defined SSH_CLIENT (
   )
 )
 
-git branch 2>NUL | call findstr /b "* " > %_temp%
+git branch 2>NUL | call findstr /b "* " > %__git_branch_temp%
 
 rem echo errorlevel is %ERRORLEVEL%
 if errorlevel 1 (set "__git_branch=")
 if errorlevel 1 goto :setprompt
 
-set /p __git_branch=<%_temp%
+set /p __git_branch=<%__git_branch_temp%
 set "__git_branch=%__git_branch: =%"
 set "__git_branch=%__git_branch:**=%"
 set "__git_branch=(%__git_branch%)"
 
 :setprompt
-if exist %_temp% del %_temp%
+if exist %__git_branch_temp% (
+  del %__git_branch_temp%
+  set "__git_branch_temp="
+)
 set "ps1=$m$_%_uh%$n:\ $e[1;34m%cwd% $+$e[00m%__git_branch% $$$s"
 prompt %ps1%
