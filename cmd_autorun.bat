b79732bb01510ffd933a216bb099dc458ba8aad0
huuthienp <htp898@uowmail.edu.au>
2024-12-02 00:40:27 +1100

    add Cygwin binaries to PATH

--- cmd_autorun.bat
+++ cmd_autorun.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set EDITOR=%USERPROFILE%\scoop\shims\lvim.cmd
+set PATH=%PATH%;%USERPROFILE%\scoop\apps\git\current\usr\bin
 
 call %USERPROFILE%\etc\cmd_prompt.bat
 call %USERPROFILE%\etc\cmd_aliases.bat
