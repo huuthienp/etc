5c738ed6583eea5626e88cc8ccb8adb1ef2244b6
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-11-19 03:04:16 +1100

    🎨 in cmd

--- cmd_autorun.bat
+++ cmd_autorun.bat
@@ -7,3 +7,6 @@ set PATH=%PATH%;%USERPROFILE%\scoop\apps\git\current\usr\bin
 
 call %USERPROFILE%\etc\cmd_prompt.bat
 call %USERPROFILE%\etc\cmd_aliases.bat
+
+@REM Enable color
+set "COLORTERM=truecolor"
