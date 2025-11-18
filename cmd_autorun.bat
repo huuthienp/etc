0ba0e766f3d0e9fbebfb2468f5767dfcdd33e772
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-11-19 04:03:04 +1100

    🎨 in cmd at launch

--- cmd_autorun.bat
+++ cmd_autorun.bat
@@ -5,8 +5,8 @@ set "DIRCMD=/A"
 set EDITOR=vscodium
 set PATH=%USERPROFILE%\scoop\apps\git\current\usr\bin;%PATH%
 
-call %USERPROFILE%\etc\cmd_prompt.bat
-call %USERPROFILE%\etc\cmd_aliases.bat
-
 @REM Enable color
 set "COLORTERM=truecolor"
+
+call %USERPROFILE%\etc\cmd_prompt.bat
+call %USERPROFILE%\etc\cmd_aliases.bat
