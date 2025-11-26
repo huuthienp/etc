6d00eb9d14102d379abf1734484940cd10fc448d
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-11-26 14:23:23 +1100

    Add aliases 251126
    - `$`: non-interactive bash
    - `lll`: list files in pager
    - `lzg`: lazygit
    - `ncdu`: rclone ncdu

--- cmd_aliases.bat
+++ cmd_aliases.bat
@@ -1,4 +1,5 @@
 @echo off
+@REM https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/doskey
 
 doskey cd=cd $* $T call %USERPROFILE%\etc\cmd_prompt.bat
 doskey pushd=pushd $* $T call %USERPROFILE%\etc\cmd_prompt.bat
@@ -11,7 +12,8 @@ doskey ld=dir /a:d $*
 doskey laa=where /f /t /r $* * 2^>NUL $T if ERRORLEVEL 2 echo Use '.' for current directory
 doskey ldd=dir /a:d /b /s $*
 
-doskey less=sh -c 'less $*'
+doskey lll=dir $* $B less
+doskey $=bash -c "$*"
 
 if DEFINED EDITOR doskey e=%EDITOR% $*
 
@@ -21,6 +23,8 @@ doskey scbl=for /f "delims=" %%G in ($1) do @set /p "_dummy=%%G" ^<NUL ^| clip
 doskey scbv=for /f "delims=" %%G in ('pwsh -c "gcb"') do @set "$*=%%G"
 doskey gcb=pwsh -c "gcb" $*
 
+@REM Git Operations
+where lazygit >nul && doskey lzg=lazygit $*
 doskey gf=git fetch
 doskey gs=git status -sb
 doskey gl=git log --oneline
@@ -30,3 +34,6 @@ doskey gc=git commit -m $*
 doskey gca=git commit -a -m $*
 doskey gd=git diff --cached --color-words $T echo. $T git diff --color-words
 doskey gds=git diff --cached --stat $T echo. $T git diff --stat
+
+@REM More Utilities
+where rclone >nul && doskey ncdu=rclone ncdu $*
